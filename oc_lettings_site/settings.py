bef5fbeccddbac9d4c150ebc502e668ebc1396f4
nicolasfrenay <*****>
2022-03-31 18:39:11 +0200

    trying to correct structure issues...

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,8 +1,8 @@
+from pathlib import Path
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'oc_lettings_site',
+    'apps.home',
     'apps.lettings',
     'apps.profiles',
 ]
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'oc_lettings_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
-
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include('apps.home.urls', namespace='home')),
     path('lettings/', include('apps.lettings.urls', namespace='lettings')),
     path('profiles/', include('apps.profiles.urls', namespace='profiles')),
     path('admin/', admin.site.urls),
