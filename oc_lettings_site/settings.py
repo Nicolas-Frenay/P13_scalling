01a6b73e1c7bef5f2fa49692b83d9461fb4c7896
nicolasfrenay <*****>
2022-04-02 21:29:01 +0200

    try something that might work : disabling heroku lib in setting, so it doesn't mess with db config... and add a staticfiles root

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,5 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_ROOT = [BASE_DIR.joinpath('staticfiles/')]
 
 # django_on_heroku.settings(locals())
